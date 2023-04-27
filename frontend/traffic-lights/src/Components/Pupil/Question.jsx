d2d7dfd48e28d35e7dc1c1110e7850beb5d25053
Wil Treasure <100243250+treasurewild@users.noreply.github.com>
2023-04-27 13:30:37 +0100

    Pupils responses use State to update button options

--- frontend/traffic-lights/src/Components/Pupil/Question.jsx
+++ frontend/traffic-lights/src/Components/Pupil/Question.jsx
@@ -2,42 +2,40 @@ import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { socket } from '../../socket';
 
-const Question = ({ question }) => {
+const Question = ({ question, answered, setAnswered }) => {
 
-    const [response, setResponse] = useState('');
+    const index = answered.map(e => e.shortId).indexOf(question.shortId);
 
     const sendResponse = (event) => {
-        setResponse(event.target.value);
+        setAnswered([
+            ...answered,
+            { shortId: question.shortId, response: event.target.value }
+        ])
         socket.emit('pupil_response', { response: event.target.value, shortId: question.shortId });
     }
 
     const displayResponse = () => {
 
-        // const index = question.responses.map(e => e.pupil).indexOf(socket.id);
-
-        // if (index === -1)
-        //     return;
-
-        //console.log(question.responses[index])
-        //setResponse(question.responses[index].response)
-
-        if (response === '1green')
-            return <Button type='button' variant='success'>Green</Button>
-        if (response === '2amber')
-            return <Button type='button' variant='warning'>Amber</Button>
-        if (response === '3red')
-            return <Button type='button' variant='danger'>Red</Button>
+        if (answered[index].response === '1green')
+            return <Button type='button' variant='success' active>Green</Button>
+        if (answered[index].response === '2amber')
+            return <Button type='button' variant='warning' active>Amber</Button>
+        if (answered[index].response === '3red')
+            return <Button type='button' variant='danger' active>Red</Button>
     }
 
     const resetResponse = (e) => {
-        socket.emit('pupil_response_reset', { response: response, shortId: question.shortId });
-        setResponse(e.target.value);
+        socket.emit('pupil_response_reset', { response: answered[index].response, shortId: question.shortId });
+
+        const copy = [...answered];
+        copy.splice(answered[index], 1);
+        setAnswered(copy);
     }
 
     return (
         <div className='m-1 p-2 bg-dark text-light'>
             <h5 className='p-2'>{question.text} </h5>
-            {response === '' ?
+            {index === -1 ?
                 <>
                     <Button className='m-1' variant='success' value='1green' onClick={sendResponse} >Green</Button>
                     <Button className='m-1' variant='warning' value='2amber' onClick={sendResponse} >Amber</Button>
