24a9f39034d74f2c1918d997f2c2346065914cc4
Wil Treasure <100243250+treasurewild@users.noreply.github.com>
2023-05-11 10:15:58 +0100

    Added to how to USe and About sections

--- frontend/traffic-lights/src/Components/Homepage/Homepage.jsx
+++ frontend/traffic-lights/src/Components/Homepage/Homepage.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import { socket } from '../../socket';
 import UserPanel from './UserPanel';
 import { getLessonPupil } from '../../Utils/lessonAPI';
+import About from './About';
+import Development from './Development';
 
 const Homepage = ({ setLesson, setLessons }) => {
 
@@ -34,13 +36,7 @@ const Homepage = ({ setLesson, setLessons }) => {
 
     return (
         <div className='main'>
-            <h1>Welcome to Traffic Lights</h1>
-            <div className='alert alert-danger'>
-                <h4>How to Use Traffic Lights</h4>
-                <p>Traffic Lights is a simple tool for class feedback when teaching remotely.</p>
-                <p>Teachers ask questions and get Green-Amber-Red feedback from the class to gauge progress and understanding. This knowledge allows them guide their lesson, differentiate tasks and informs future learning.</p>
-                <p>Teachers can save lesson data for reference, and even create templates to reuse for future lessons.</p>
-            </div>
+            <About />
             {user?.accessToken ?
                 <div className='d-grid'>
                     <Button variant='warning' className='mb-3' size='lg' onClick={() => navigate('/teacher')}>Go to Lessons</Button>
@@ -61,12 +57,7 @@ const Homepage = ({ setLesson, setLessons }) => {
                     {message && <p className='m-1 alert alert-danger'>{message}</p>}
                 </Form>
             </div>
-            <div className='alert alert-secondary'>
-                <p >Traffic Lights is a demo App created by Wil Treasure.</p>
-                <p><strong>Please don't add sensitive personal information!</strong></p>
-                <p>It's created using the following technologies: ReactJS, Mongoose, Express, Node, Short-Unique-ID, and Socket.io.
-                    <br />It uses Bootstrap and React Bootstrap for styling.</p>
-            </div>
+            <Development />
         </div>
     )
 }
